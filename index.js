d6afaae9d8fad1b91993ff6aa442050b29e1db36
NiKole Maxwell <alertnikole@gmail.com>
2018-10-08 00:15:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,16 +23,15 @@ function add2(n) {
 
 var funkyFunction = function() {
   return function() {
-    var word = "FUNKY"
-    return word
+    return "FUNKY!"
   }
-} 
+}
+ 
+// We want 'funkyFunction' on the line below to return a function that returns "FUNKY!" -- how can we accomplish that?
+// NOTE: To pass this final test, you only need to modify the code below this line.
 
-
-// We want to set theFunk equal to "FUNKY!" using our funkyFunction.
-// NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
-
+var theFunk = funkyFunction()
+theFunk = theFunk()
 
 /*global describe, it */
 
